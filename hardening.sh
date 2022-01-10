620efc95131d9dfb3152fa672b98cce9c14fffca
alteregog1 <88210725+alteregog1@users.noreply.github.com>
2022-01-10 18:46:28 +0700

    Create hardening.sh

---
+++ hardening.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+echo "========================================================="
+echo "=       Updating OS                                     ="
+echo "========================================================="
+apt-get update
+apt-get upgrade
+echo "========================================================="
+echo "=       Creating New Username                           ="
+echo "========================================================="
+echo "Input new Username:"
+read new_username
+adduser $new_username
+usermod -aG sudo $new_username
+echo "========================================================="
+echo "=       Locking ROOT user for ssh login                 ="
+echo "========================================================="
+sed -i 's/PermitRootLogin yes/PermitRootLogin no' "/etc/ssh/sshd_config"
+echo "========================================================="
+echo "=      Change Default SSH Port to new One               ="
+echo "========================================================="
+echo "Input new port number for SSH:"
+read new_port
+sed -i 's/#Port 22/Port $new_port' "/etc/ssh/sshd_config"
+echo "========================================================="
+echo "=      Restarting SSH                                   ="
+echo "========================================================="
+service ssh restart
+echo "========================================================="
+echo "=      Adding New Port to Firewall                      ="
+echo "========================================================="
+ufw allow $new_port
+ufw enable
+echo "========================================================="
+echo "=     Disabling root user                               ="
+echo "========================================================="
+passwd -l root
